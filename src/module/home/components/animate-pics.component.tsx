47ed46af44bd46eccebdb5a4f3cbb6ff735faa05
Siavash <siavash.aghazadeh@gmail.com>
2024-03-01 19:13:04 +0330

    fix image sizes

--- src/module/home/components/animate-pics.component.tsx
+++ src/module/home/components/animate-pics.component.tsx
@@ -1,15 +1,6 @@
 'use client';
 import React from 'react';
-
-import Coin1 from '~/assets/images/svg/coin-1.svg';
-import Coin2 from '~/assets/images/svg/coin-2.svg';
-import Coin3 from '~/assets/images/svg/coin-3.svg';
-import Coin4 from '~/assets/images/svg/coin-4.svg';
-
-import Coin1Dark from '~/assets/images/svg/coin-1-dark.svg';
-import Coin2Dark from '~/assets/images/svg/coin-2-dark.svg';
-import Coin3Dark from '~/assets/images/svg/coin-3-dark.svg';
-import Coin4Dark from '~/assets/images/svg/coin-4-dark.svg';
+import Image from 'next/image';
 
 import Ellipse1 from '~/assets/images/svg/ellipse.svg';
 import Ellipse2 from '~/assets/images/svg/ellipse-2.svg';
@@ -18,17 +9,62 @@ import Ellipse3 from '~/assets/images/svg/ellipse-3.svg';
 function AnimatePics() {
   return (
     <div className="absolute left-0 top-0 w-full">
-      <Coin1 className="animate-UpDownSpin absolute -top-5 right-[7%] z-20 dark:hidden md:hidden" />
-      <Coin1Dark className="animate-UpDownSpin absolute -top-5 right-[7%] z-20 hidden dark:block dark:md:hidden" />
-
-      <Coin2 className="animate-UpDownSpin absolute right-[12%] top-[250px] z-20 dark:hidden md:hidden" />
-      <Coin2Dark className="animate-UpDownSpin absolute right-[12%] top-[250px] z-20 hidden dark:block dark:md:hidden" />
-
-      <Coin3 className="animate-UpDownSpin absolute left-[10%] top-[80px] z-20 dark:hidden md:hidden" />
-      <Coin3Dark className="animate-UpDownSpin absolute left-[10%] top-[80px] z-20 hidden dark:block dark:md:hidden" />
-
-      <Coin4 className="animate-UpDownSpin absolute left-[14%] top-[320px] z-20 dark:hidden md:hidden" />
-      <Coin4Dark className="animate-UpDownSpin absolute left-[14%] top-[320px] z-20 hidden dark:block dark:md:hidden" />
+      <Image
+        src="./assets/images/png/coin-1.png"
+        alt="coin"
+        width={307}
+        height={307}
+        className="animate-UpDownSpin absolute -top-5 right-[7%] z-20 dark:hidden md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-1-dark.png"
+        alt="coin"
+        width={307}
+        height={307}
+        className="animate-UpDownSpin absolute -top-5 right-[7%] z-20 hidden dark:block dark:md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-2.png"
+        alt="coin"
+        width={215}
+        height={215}
+        className="animate-UpDownSpin absolute right-[12%] top-[250px] z-20 dark:hidden md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-2-dark.png"
+        alt="coin"
+        width={215}
+        height={215}
+        className="animate-UpDownSpin absolute right-[12%] top-[250px] z-20 hidden dark:block dark:md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-3.png"
+        alt="coin"
+        width={178}
+        height={180}
+        className="animate-UpDownSpin absolute left-[10%] top-[80px] z-20 dark:hidden md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-3-dark.png"
+        alt="coin"
+        width={178}
+        height={180}
+        className="animate-UpDownSpin absolute left-[10%] top-[80px] z-20 hidden dark:block dark:md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-4.png"
+        alt="coin"
+        width={135}
+        height={138}
+        className="animate-UpDownSpin absolute left-[14%] top-[320px] z-20 dark:hidden md:hidden"
+      />
+      <Image
+        src="./assets/images/png/coin-4-dark.png"
+        alt="coin"
+        width={135}
+        height={138}
+        className="animate-UpDownSpin absolute left-[14%] top-[320px] z-20 hidden dark:block dark:md:hidden"
+      />
 
       <Ellipse1 className="absolute left-0 top-0 z-10" />
       <Ellipse2 className="absolute right-0 top-0 z-10" />
